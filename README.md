01efbd074ad4b1906a1ec495e910451909893747
snehilsin <110235521+snehilsin@users.noreply.github.com>
2023-12-30 13:50:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# LaserSlides
-LaserSlides, my GSOC 2024 project linked with LiquidGalaxy, is a Flutter app designed to enrich user experiences within LiquidGalaxy's environment. Developed in collaboration with LiquidGalaxy organization, this app introduces advanced tools aimed at optimizing workflows and user interactions.
+# my_first_app
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Vinod Kumar\\Downloads\\flutter_windows_3.16.4-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Vinod Kumar\\OneDrive\\Desktop\\Flutter Projects\\my_first_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Vinod Kumar\\Downloads\\flutter_windows_3.16.4-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Vinod Kumar\\OneDrive\\Desktop\\Flutter Projects\\my_first_app"
+  "FLUTTER_ROOT=C:\\Users\\Vinod Kumar\\Downloads\\flutter_windows_3.16.4-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Vinod Kumar\\OneDrive\\Desktop\\Flutter Projects\\my_first_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Vinod Kumar\\OneDrive\\Desktop\\Flutter Projects\\my_first_app"
+  "FLUTTER_TARGET=C:\\Users\\Vinod Kumar\\OneDrive\\Desktop\\Flutter Projects\\my_first_app\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC81NGE3MTQ1MzAzZjBkZDlkMGY5MzQyNGEyZTEyNGViNGFiZWY1MDkxLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Vinod Kumar\\OneDrive\\Desktop\\Flutter Projects\\my_first_app\\.dart_tool\\package_config.json"
+)
